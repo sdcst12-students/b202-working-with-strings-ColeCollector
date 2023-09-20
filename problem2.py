ca60b8cd5adee5243a7ad8c4523e55870553abae
ColeCollector <colen827@deltalearns.ca>
2023-09-20 13:34:59 -0700

    a

--- problem2.py
+++ problem2.py
@@ -3,3 +3,16 @@
 Retrieve the contents of the sd.deltasd.bc.ca webpage.
 Remove all of the HTML and display just the real contents of the page.
 """
+
+
+import requests
+import re
+
+x = requests.get("https://sd.deltasd.bc.ca")
+split = x.text.split(">")
+
+
+x = re.sub("<.+>","",x.text)
+x = re.sub("{.+}","",x.text)
+
+print(x)
